7758f1986071d4438d4dc89753792e5ba60c6354
HotelBudapest <arianislam.010@gmail.com>
2024-12-24 06:24:18 +0600

    patch: Better Keyword Detection and Styling Added

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
-from extractor import extract_keywords_from_text
+from extractor import extract_phrases
+import re
 
 app = Flask(__name__)
+
+def highlight_phrases(text, phrases):
+    phrases = sorted(phrases, key=len, reverse=True)
+
+    for phrase in phrases:
+        pattern = r'\b' + re.escape(phrase) + r'\b'
+        repl = rf'<span class="highlight">{phrase}</span>'
+        text = re.sub(pattern, repl, text, flags=re.IGNORECASE)
+
+    return text
 
 @app.route('/')
 def index():
@@ -16,10 +27,9 @@ def extract_keywords():
     if file.filename == '':
         return redirect(request.url)
     text = file.read().decode('utf-8')
-    keywords = extract_keywords_from_text(text)
-
-    return render_template('results.html', keywords=keywords)
+    phrases = extract_phrases(text, keep_single_word=False)
+    highlighted_text = highlight_phrases(text, phrases)
+    return render_template('results.html', highlighted_text=highlighted_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- extractor.py
+++ extractor.py
@@ -1,15 +1,39 @@
-import sys
 import spacy
 
-def extract_keywords_from_text(text):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(text)
-    interesting_pos = {"NOUN", "PROPN", "ADJ"}
+def customize_stopwords(nlp):
+    custom_stops = {"looking", "making", "work", "people"}
+    for w in custom_stops:
+        nlp.vocab[w].is_stop = True
 
-    keywords = []
+def extract_phrases(text, model="en_core_web_sm", keep_single_word=False):
+    nlp = spacy.load(model)
+    customize_stopwords(nlp)
+    doc = nlp(text)
+
+    phrases = []
+    accum = []
+
+    def flush_accumulator(acc):
+        if not acc:
+            return
+        if len(acc) > 1:
+            phrase = " ".join(acc)
+            phrases.append(phrase.lower())
+        elif len(acc) == 1 and keep_single_word:
+            phrases.append(acc[0].lower())
+        acc.clear()
 
     for token in doc:
-        if not token.is_stop and token.is_alpha and token.pos_ in interesting_pos:
-            keywords.append(token.lemma_.lower())
+        if (
+            token.pos_ in {"PROPN", "NOUN", "ADJ"} 
+            and not token.is_stop 
+            and token.is_alpha
+        ):
+            accum.append(token.text)
+        else:
+            flush_accumulator(accum)
+    
+    flush_accumulator(accum)
 
-    return set(keywords)
+    unique_phrases = sorted(set(phrases))
+    return unique_phrases
